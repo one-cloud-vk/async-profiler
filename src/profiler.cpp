3b676753d4bd5f83873baa78806d5fc1a9e6df75
Morozov Maksim <maksim.morozov@vkteam.ru>
2025-11-07 11:20:29 +0300

    Try to resolve error for GetMethodDeclaringClass separatly

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -1120,10 +1120,7 @@ Error Profiler::stop(bool restart) {
     _jfr.stop();
     unlockAll();
 
-    if (!restart) {
-        FdTransferClient::closePeer();
-    }
-
+    FdTransferClient::closePeer();
     _state = IDLE;
     return Error::OK;
 }
@@ -1674,7 +1671,6 @@ Error Profiler::expire(Arguments& args, bool restart) {
     }
 
     if (restart) {
-        args._fdtransfer = false;  // keep the previous connection
         args._file_num++;
         return start(args, true);
     }
